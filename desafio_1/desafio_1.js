a355ac0c83c024b921e63a18c7f0ccd9b204467f
Martin Maruca <martinmaruca@gmail.com>
2022-05-27 19:24:25 -0300

    primer commit

---
+++ desafio_1/desafio_1.js
@@ -0,0 +1,30 @@
+
+// a)
+
+let entrada = prompt("DE QUE EQUIPO DE FUTBOL ERES HINCHA? 1-BOCA 2-RIVER");
+if ((entrada.toLowerCase() === "river")) {
+    alert("SE NOTA QUE SABES DE FUTBOL");
+} else {
+    alert("LO SIENTO POR VOS!!!");
+}
+
+
+// b)
+
+//bucle while
+
+let i = 1;
+
+while(i < 11) {
+    console.log(i);
+    i++
+}  
+
+// bucle for
+ 
+let z 
+
+for( z = 0; z<11; z++) {
+    console.log(z);
+}   
+
